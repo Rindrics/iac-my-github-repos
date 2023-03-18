f4b760a28443c64871a0bdc642148b8227934101
Akira Hayashi <smxshxishxad@me.com>
2023-03-19 06:54:37 +0900

    :wrench: Configure github provider

---
+++ gtd-django/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-github-repos"
+    key            = "gtd-django.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~>5.18"
+    }
+  }
+}
+
+provider "github" {
+  token = var.GITHUB_TOKEN
+}
