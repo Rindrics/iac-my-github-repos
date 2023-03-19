7905f3b0e108ceb904e7abc3eacc8b966c73b2bb
Akira Hayashi <smxshxishxad@me.com>
2023-03-20 01:12:16 +0900

    :sparkles: Provision 'gtd-django' using module

--- repositories/gtd-django/main.tf
+++ repositories/gtd-django/main.tf
@@ -18,22 +18,6 @@ provider "github" {
   token = var.GITHUB_TOKEN
 }
 
-resource "github_repository" "this" {
-  name         = "gtd-django"
-  description  = "trict GTD app written by Django"
-  visibility   = "public"
-  has_projects = true
-  has_issues   = true
-}
-
-resource "github_issue" "django_scaffold" {
-  repository = github_repository.this.name
-  title      = "Add Django scaffold"
-  body       = ""
-}
-
-resource "github_issue" "add_capture" {
-  repository = github_repository.this.name
-  title      = "Add feature 'capture'"
-  body       = ""
+module "gtd_django" {
+  source = "../../modules/gtd"
 }
